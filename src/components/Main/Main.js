a528e483a9d051d5236b22d5a1819dfb8724e35e
kubryak <al.lcn@yandex.ru>
2023-10-25 17:00:09 +0500

    feat: реализовать отображение карточек в результате поиска и профиле

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,18 +5,32 @@ import Profile from '../Profile/Profile'
 import { api } from '../../utils/api.js'
 
 export default function Main() {
+  const [searchResult, setSearchResult] = useState([])
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [searchStatus, setSearchStatus] = useState('start');
+
+  const handleCardClick = (user) => {
+    setSelectedUser(user);
+  }
 
   function getUsers(search) {
+    setSearchStatus('loading')
+    setSelectedUser(null)
     api.getUsers(search)
       .then((res) => {
-        console.log(res)
+        setSearchResult(res)
+        setSearchStatus(res.length > 0 ? 'success' : 'empty')
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        setSearchStatus('error')
+      })
   }
+
   return (
     <div className='main'>
-      <Sidebar onSearch={getUsers} />
-      <Profile />
+      <Sidebar onSearch={getUsers} searchResult={searchResult} onCardClick={handleCardClick} searchStatus={searchStatus} setSelectedUser={setSelectedUser} setSearchStatus={setSearchStatus}/>
+      <Profile user={selectedUser} />
     </div>
   )
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,37 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Sidebar.scss'
 import card_img from '../../image/sidebar__card-img.png'
 
-export default function Sidebar({ onSearch }) {
-  const [search, setSearch] = useState('');
+export default function Sidebar({ onSearch, searchResult, onCardClick, searchStatus, setSelectedUser, setSearchStatus}) {
+  const [query, setQuery] = useState('');
+  const [selectedUserId, setSelectedUserId] = useState(null);
+
+  useEffect(() => {
+    setSelectedUserId(null)
+  }, [searchResult])
+
+  let content;
+
+  if (searchStatus === 'start' || query === '') {
+    content = <p className='sidebar__text-result'>начните поиск</p>
+    setSelectedUser(null);
+    setSearchStatus('start');
+  } else if (searchStatus === 'loading') {
+    content = <p className='sidebar__text-result'>Загрузка...</p>
+  } else if (searchStatus === 'success' && searchResult.length > 0) {
+    content = (
+      <div className='sidebar__cards'>
+        {searchResult.map(user => (
+          <div key={user.id} className={`sidebar__card ${user.id === selectedUserId ? 'sidebar__card_type_active' : ''}`} onClick={() => handleCardClick(user)}>
+            <img className='sidebar__card-img' src={card_img} alt='Фотография сотрудника' />
+            <div className={`sidebar__card-info ${user.id === selectedUserId ? 'sidebar__card-info_type_active' : ''}`}>
+              <p className='sidebar__card-name'>{user.name}</p>
+              <p className='sidebar__card-email'>{user.email}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  } else if (searchStatus === 'error') {
+    content = <p className='sidebar__text-result'>ошибка на сервере</p>
+  }
+  else {
+    content = <p className='sidebar__text-result'>ничего не найдено</p>
+  }
+
+  const handleCardClick = (user) => {
+    setSelectedUserId(user.id);
+    onCardClick(user);
+  }
 
   function onSubmit(e) {
     e.preventDefault();
-    onSearch(search);
+    onSearch(query);
   }
 
   return (
     <div className='sidebar'>
       <h2 className='sidebar__title'>Поиск сотрудников</h2>
       <form onSubmit={onSubmit}>
-        <input onChange={(e) => setSearch(e.target.value)} className='sidebar__input' type='text' placeholder='Введите Id или имя' />
+        <input onChange={(e) => setQuery(e.target.value)} className='sidebar__input' type='text' placeholder='Введите Id или имя' />
       </form>
       <h2 className='sidebar__title-result'>Результаты</h2>
-      {/* <p className='sidebar__text-result'>начните поиск</p> */}
-      <div className='sidebar__card'>
-        <img className='sidebar__card-img' src={card_img} alt='Фотография сотрудника' />
-        <div className='sidebar__card-info'>
-          <p className='sidebar__card-name'>Bret</p>
-          <p className='sidebar__card-email'>Sincere@april.biz</p>
-        </div>
-      </div>
-      <div className='sidebar__card sidebar__card_type_active'>
-        <img className='sidebar__card-img' src={card_img} alt='Фотография сотрудника' />
-        <div className='sidebar__card-info sidebar__card-info_type_active'>
-          <p className='sidebar__card-name'>Bret</p>
-          <p className='sidebar__card-email'>Sincere@april.biz</p>
-        </div>
-      </div>
+      {content}
     </div>
   )
 }
